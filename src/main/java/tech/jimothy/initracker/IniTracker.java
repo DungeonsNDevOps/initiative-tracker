4c4b166fc1fe3a1e5dab4197ad5a848af001f4c1
jimothy <timothynewton1998@gmail.com>
2023-06-21 14:29:18 -0400

    changed directories

---
+++ src/main/java/tech/jimothy/initracker/IniTracker.java
@@ -0,0 +1,7 @@
+package tech.jimothy.initracker;
+
+public class IniTracker {
+    public static void main(String[] args){
+        Window.createWindow(args);
+    }
+}
---
+++ src/main/java/tech/jimothy/initracker/Window.java
@@ -0,0 +1,24 @@
+package tech.jimothy.initracker;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Window extends Application{
+    @Override
+    public void start(Stage stage) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/campaign-page.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Initiative Tracker");
+        stage.show();
+
+    }
+    public static void createWindow(String[] args){
+        launch(args);
+    }
+}
