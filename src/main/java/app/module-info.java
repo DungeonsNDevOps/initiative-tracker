623e4b4257149f72680e3ea296546db5ee49e0a5
jimothy <timothynewton1998@gmail.com>
2023-06-21 14:27:12 -0400

    deleted module-info

--- src/main/java/app/module-info.java
+++
@@ -1,9 +0,0 @@
-module app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires transitive javafx.graphics;
-
-    opens app.tech.jimothy.initracker to javafx.fxml;
-    exports app.tech.jimothy.initracker;
-}
