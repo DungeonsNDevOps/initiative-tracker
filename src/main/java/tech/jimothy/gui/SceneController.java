06415b8eb596898ececa966e4442fc6112623186
jimothy <timothynewton1998@gmail.com>
2023-06-22 11:58:34 -0400

    scene controller for switching between scenes

---
+++ src/main/java/tech/jimothy/gui/SceneController.java
@@ -0,0 +1,36 @@
+package tech.jimothy.gui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class SceneController {
+     
+    private Stage stage;
+    private Scene scene;
+    @FXML private Parent root;
+    @FXML MenuButton characterSelection;
+
+    public void goToCampaignPage(ActionEvent event) throws IOException{
+        root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/campaign-page.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToCreateCampaign(ActionEvent event) throws IOException{
+        stage = (Stage)root.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/create-campaign-page.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
