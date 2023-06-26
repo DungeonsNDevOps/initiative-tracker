e6bceefc6d1b97b2c490bb40e6919391e3a0b440
jimothy <timothynewton1998@gmail.com>
2023-06-26 00:03:05 -0400

    Added db package import

--- src/main/java/tech/jimothy/gui/Window.java
+++ src/main/java/tech/jimothy/gui/Window.java
@@ -3,16 +3,17 @@ package tech.jimothy.gui;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tech.jimothy.db.Database;
 
 public class Window extends Application{
 
     @Override
     public void start(Stage stage) throws IOException{
+        Database database = new Database("./sqlite/inibase");
         Parent root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/campaign-page.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
