a44054b34fe7035a77b8edd4a7b55e89ba9c2851
jimothy <timothynewton1998@gmail.com>
2023-07-12 13:41:02 -0400

    tested database

--- src/main/java/tech/jimothy/gui/Window.java
+++ src/main/java/tech/jimothy/gui/Window.java
@@ -1,6 +1,9 @@
 package tech.jimothy.gui;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,12 +11,19 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import tech.jimothy.db.Database;
+import tech.jimothy.db.Table;
 
 public class Window extends Application{
 
     @Override
-    public void start(Stage stage) throws IOException{
-        Database database = new Database("./sqlite/inibase");
+    public void start(Stage stage) throws IOException, SQLException{
+        // //DATABASE TEST -------------------------------------------
+        // Database database = new Database("./sqlite/inibase");
+        // Table table = database.query("SELECT * FROM characters");
+        // String nameResult = table.get(3, "name");
+        // String bonusResult = table.get(3, "bonus");
+        // System.out.println("\nCharacter Name: " + nameResult + "\n" + "Bonus: " + bonusResult + "\n");
+        // //---------------------------------------------------------
         Parent root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/campaign-page.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
