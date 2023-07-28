9ce6782d6ca80f85c814719abc779ecac807e85c
jimothy <timothynewton1998@gmail.com>
2023-07-27 23:26:18 -0400

    minor changes

--- src/main/java/tech/jimothy/gui/custom/CharacterWidget.java
+++ src/main/java/tech/jimothy/gui/custom/CharacterWidget.java
@@ -2,19 +2,18 @@ package tech.jimothy.gui.custom;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class CharacterWidget extends HBox {
     
-    public CharacterWidget(int spacing, String name, String bonus){
+    public CharacterWidget(int spacing, int width, String name, String bonus){
         super(spacing);
-
-        String characterName = charactersTable.get(i, "name");
 
         HBox characterBox = new HBox(20.0);
         characterBox.setStyle("-fx-border-style: solid;" + 
                                 "-fx-border-width: 1px;");
-        characterBox.setMaxWidth(charactersVBox.getPrefWidth()*.8);
-        Label characterNameLabel = new Label(characterName);
+        characterBox.setMaxWidth(width);
+        Label characterNameLabel = new Label(name);
 
         characterBox.getChildren().add(characterNameLabel);
     }
