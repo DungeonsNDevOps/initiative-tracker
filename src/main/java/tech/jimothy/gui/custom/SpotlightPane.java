ec9a6a1208b0113221fc0469a1c1b83cf528c24f
jimothy <timothynewton1998@gmail.com>
2023-08-16 00:09:20 -0400

    Spotlight basic functionality finished

--- src/main/java/tech/jimothy/gui/custom/SpotlightPane.java
+++ src/main/java/tech/jimothy/gui/custom/SpotlightPane.java
@@ -21,12 +21,16 @@ public class SpotlightPane extends Pane{
 
     /**The child that's in the spotlight */
     private Node spotlightChild;
-    //The index of the child currntly within the spotlight. 
+    /** The index of the child currntly within the spotlight. This index is in relation
+     * to this spotlightpane's list of children. 
+    */
     private int spotlightIndex = 0;
-    //this X coordinate of the static spotlight position
-    private double focusPosX;
-    //the Y coordinate of the static spotlight position
-    private double focusPosY;
+    /** this X coordinate of the static spotlight position */
+    private double spotlightPosX;
+    /** the Y coordinate of the static spotlight position */
+    private double spotlightPosY;
+    /* defines the amount of times layoutChildren() has been executed */
+    private int layoutChildrenCount = 0;
 
 
 
@@ -47,55 +51,91 @@ public class SpotlightPane extends Pane{
         //
         this.setOnKeyPressed(event -> {
             if(event.getCode() == KeyCode.DOWN){
-                this.shiftChildrenDown();
-            } else if (event.getCode() == KeyCode.UP){
                 this.shiftChildrenUp();
+            } else if (event.getCode() == KeyCode.UP){
+                this.shiftChildrenDown();
             }
         });
-
     }
 
     @Override
     protected void layoutChildren(){
         super.layoutChildren();
+        
 
         ObservableList<Node> children = this.getChildren();
 
         double childWidth = this.getMinWidth()*this.widthFactor;
 
-        this.focusPosX = (this.getMinWidth()/2.0) - (childWidth/2);
-        double offsetY = this.focusPosY = this.getMinHeight()/2.0;
+        this.spotlightPosX = (this.getWidth()/2.0) - (childWidth/2);
+        this.spotlightPosY = this.getHeight()/2.0;
 
+        //The index of child in the proceeding for loop
         int childIndex = 0;
-        for (Node child : children){
-            if(child instanceof Region){
-                if(childIndex == 0){
-                    this.spotlightChild = child;
+        //*check to ensure that this is the first time we're running layoutChildren()
+        //*since we don't want to run this bit of code any subsequent times.
+        if(layoutChildrenCount < 1){
+            for (Node child : children){
+                if (child instanceof Region){
+                    ((Region)child).setMinWidth(childWidth);
                 }
-                ((Region)child).setMinWidth(childWidth);
-                child.relocate(this.focusPosX, offsetY);
-                offsetY += gap;                
-            }
-            childIndex += 1; 
-        }
-    }
+                if (childIndex == this.spotlightIndex){
+                    child.relocate(this.spotlightPosX, this.spotlightPosY);
+                    child.setScaleX(2);
+                    child.setScaleY(2);
+                } else{
 
-    private void shiftChildrenDown(){
+                    double distance = 0;
+                    for (int i = 0; i < childIndex; i++){
+                        if (children.get(i) instanceof Region){
+                            distance += ((Region)children.get(i)).getHeight() + this.gap;
+                        }
+                    }
+
+                    child.relocate(this.spotlightPosX, this.spotlightPosY + distance);
+                }
+                childIndex += 1;
+            }
+        layoutChildrenCount += 1;
+    }
+}
+    /**
+     * Method shifts children up, placing a the next child in line in the spotlight position
+     */
+    private void shiftChildrenUp(){
         ObservableList<Node> children = this.getChildren();
 
-        //Check to make sure we won't go out of bounds
+        //*Check to make sure we won't go out of bounds
         if (spotlightIndex + 1 < children.size()){
+
+            /*
+             * Changes the index of the child that should be in the spotlight positon.
+             * This is what determines whether children will be shifted up or down. 
+             * In this case, since we are adding, children will be shifted up. 
+             */
             spotlightIndex += 1;
             this.spotlightChild = children.get(spotlightIndex);
 
+            /*
+             * Iterate through each child in this spotlightpane's list of children, 
+             * relocating them as needed
+             */
             for(int i = 0; i < children.size(); i++){
+                //The current child of the iteration
                 Node child = children.get(i);
+
+                /*
+                 * If index of the current child is below the spotlight index 
+                 * (spatially above the child in the spotlight position), we want to find the 
+                 * calculate how far away this child should be from the spotlight position and use 
+                 * that distance to position the current child
+                 */
                 if (i < spotlightIndex){
 
                     //The distance between the current child per iteration and the spotlight position
                     double distance = 0;
-                    //iterate from the current child to the spotlight index, counting the distance
-                    //this is to get the current distance of the current child away from the 
+                    //iterate from the current child to the spotlight index, counting each gap and
+                    //and height of each child - this is to get the current distance of the current child away from the 
                     //spotlight position
                     for(int ii = i; ii < this.spotlightIndex; ii++){
                         /*check to make sure child is an instance of region
@@ -107,16 +147,35 @@ public class SpotlightPane extends Pane{
                         }
                         
                     }
-                    if (child instanceof Region){
-                        ((Region)child).relocate(this.focusPosX, this.focusPosY-distance);
-                        System.out.println("/");
-                    }
+                    /*
+                     * Now that we have a distance away from the spotlight positon for this 
+                     * particular child, we can translate this child on the Y axis for the 
+                     * calculated distance. We use the Node class's method relocate() to 
+                     * set the new position of the child. We also ensure that the scale factor
+                     * is set to 1 (100%)
+                     */
+                    child.relocate(this.spotlightPosX, this.spotlightPosY-distance);
+                    child.setScaleX(1);
+                    child.setScaleY(1);
+                    System.out.println("/");
 
-                }else if (i == spotlightIndex){
-                    if(child instanceof Region){
-                        ((Region)child).relocate(this.focusPosX, this.focusPosY);
-                        System.out.println("*");
-                    }
+                /*
+                 * else, if the index of the current child is the index of the child that 
+                 * should be at the spotlight index, then set its position to the spotlight position
+                 * and scale the child by a factor of 2 (200%)
+                 */
+                }else if (i == spotlightIndex){ 
+                    child.relocate(this.spotlightPosX, this.spotlightPosY);
+                    child.setScaleX(2);
+                    child.setScaleY(2);
+                    System.out.println("*");
+
+                /*
+                 * Else (if the index of the current child is greater than the index
+                 * of the child at the spotlight position), we want to again count the distance
+                 * between the current child and the spotlight position, using this said distance
+                 * to position the child away from the spotlight position
+                 */
                 } else{
                     //The distance between the current child per iteration and the spotlight position
                     double distance = 0;
@@ -125,29 +184,82 @@ public class SpotlightPane extends Pane{
                      */
                     for(int ii = spotlightIndex; ii < i; ii++){
                         if(children.get(ii) instanceof Region){
-                            distance += ((Region)child).getHeight() + this.gap;
+                            distance += ((Region)children.get(ii)).getHeight() + this.gap;
 
                         }
                     }
 
-                    if(child instanceof Region){
-                        ((Region)child).relocate(this.focusPosX, this.focusPosY + distance);
-                        System.out.println("/");
-                    }
-                }
+                    child.relocate(this.spotlightPosX, this.spotlightPosY + distance);
+                    child.setScaleX(1);
+                    child.setScaleY(1);
+                    System.out.println("/");
 
-            }
-            
+                }
+            }    
         }
     }
 
-    private void shiftChildrenUp(){
+    /**
+     * Method shifts children down, placing the next child in line in the spotlight position
+     */
+    private void shiftChildrenDown(){
         ObservableList<Node> children = this.getChildren();
 
-        //Check to make sure we won't go out of bounds
+        //*Check to make sure we won't go out of bounds
         if (spotlightIndex > 0){
             spotlightIndex -= 1;
-            this.spotlightChild = children.get(spotlightIndex); 
+            this.spotlightChild = children.get(spotlightIndex);
+
+            for(int i = 0; i < children.size(); i++){
+                Node child = children.get(i);
+                if (i < spotlightIndex){
+
+                    //The distance between the current child per iteration and the spotlight position
+                    double distance = 0;
+                    //iterate from the current child to the spotlight index, counting each gap and
+                    //and height of each child - this is to get the current distance of the current child away from the 
+                    //spotlight position
+                    for(int ii = i; ii < this.spotlightIndex; ii++){
+                        /*check to make sure child is an instance of region
+                         * so we can cast it to region and get it's height for
+                         * calculating distance.
+                         */
+                        if (children.get(ii) instanceof Region){
+                           distance += ((Region)children.get(ii)).getHeight() + this.gap;
+                        }
+                        
+                    }
+                    child.relocate(this.spotlightPosX, this.spotlightPosY-distance);
+                    child.setScaleX(1);
+                    child.setScaleY(1);
+                    System.out.println("/");
+
+
+                }else if (i == spotlightIndex){
+                    child.relocate(this.spotlightPosX, this.spotlightPosY);
+                    child.setScaleX(2);
+                    child.setScaleY(2);
+                    System.out.println("*");
+                } else{
+                    //The distance between the current child per iteration and the spotlight position
+                    double distance = 0;
+                    /*Iterate from the child at the spotlightIndex through to the 
+                     * current child per iteration, counting the distance.
+                     */
+                    for(int ii = spotlightIndex; ii < i; ii++){
+                        if(children.get(ii) instanceof Region){
+                            distance += ((Region)children.get(ii)).getHeight() + this.gap;
+
+                        }
+                    }
+
+                    child.relocate(this.spotlightPosX, this.spotlightPosY + distance);
+                    child.setScaleX(1);
+                    child.setScaleY(1);
+                    System.out.println("/");
+
+                }
+            }    
         }
     }
 }
