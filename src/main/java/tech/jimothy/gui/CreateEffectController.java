68df954f2df2b69ab70d8117403f4bae9a283611
jimothy <timothynewton1998@gmail.com>
2023-09-01 16:06:50 -0400

    Created controller for create-effect-page

---
+++ src/main/java/tech/jimothy/gui/CreateEffectController.java
@@ -0,0 +1,22 @@
+package tech.jimothy.gui;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class CreateEffectController {
+    @FXML private TextField nameTextField;
+    @FXML private TextArea descTextArea;
+    @FXML private TextField durationTextField;
+
+    @FXML
+    protected void initialize(){
+        ;
+    }
