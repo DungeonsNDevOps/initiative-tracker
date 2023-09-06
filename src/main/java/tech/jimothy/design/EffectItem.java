afec5547bbf857fc8a4ceee086fd1ff3f8839f51
jimothy <timothynewton1998@gmail.com>
2023-09-06 16:16:37 -0400

    add effect item type

---
+++ src/main/java/tech/jimothy/design/EffectItem.java
@@ -0,0 +1,23 @@
+package tech.jimothy.design;
+
+import javafx.scene.control.ListView;
+import tech.jimothy.db.Database;
+import tech.jimothy.db.Table;
+
+public class EffectItem implements ItemType{
+    @Override
+    public void populateItems(ListView<Object> listView){
+        Database database = new Database("./sqlite/inibase");
+
+        Table itemsTable = database.query("SELECT * FROM effects");
+        
+        for (int i = 0; i < itemsTable.getSize(); i++){
+            String effectDesc = itemsTable.get(i, "desc");
+            int effectID = Integer.valueOf(itemsTable.get(i, "id"));
+            String effectName = itemsTable.get(i, "name");
+            int effectDuration = Integer.valueOf(itemsTable.get(i, "duration"));
+
+            listView.getItems().add(new Effect(effectDesc, effectName, effectID, effectDuration));        
+        }
+    }
+}
