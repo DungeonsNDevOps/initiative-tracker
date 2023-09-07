0e786af0c98d62280b9bbe650c29fbda28d0efc2
jimothy <timothynewton1998@gmail.com>
2023-09-07 12:50:45 -0400

    DB experimentation

--- src/main/java/tech/jimothy/gui/Window.java
+++ src/main/java/tech/jimothy/gui/Window.java
@@ -8,18 +8,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tech.jimothy.db.Database;
+import tech.jimothy.db.Table;
 
 public class Window extends Application{
 
     @Override
     public void start(Stage stage) throws IOException, SQLException{
-        // //DATABASE TEST -------------------------------------------
-        // Database database = new Database("./sqlite/inibase");
-        // Table table = database.query("SELECT * FROM characters");
-        // String nameResult = table.get(3, "name");
-        // String bonusResult = table.get(3, "bonus");
-        // System.out.println("\nCharacter Name: " + nameResult + "\n" + "Bonus: " + bonusResult + "\n");
-        // //---------------------------------------------------------
+        //DATABASE TEST -------------------------------------------
+        Database database = new Database("./sqlite/inibase");
+        Table table = database.query("SELECT name FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%';");
+        for(int i = 0; i < table.getSize(); i++){
+            System.out.println(table.get(i, "name"));
+        }
+        //---------------------------------------------------------
         Parent root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/campaign-page.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
