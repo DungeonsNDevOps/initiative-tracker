df7f02634a6feca634c5efb1f0e83693899036d7
jimothy <timothynewton1998@gmail.com>
2023-09-20 15:57:45 -0400

    refactored to use static db URL from config

--- src/main/java/tech/jimothy/gui/Window.java
+++ src/main/java/tech/jimothy/gui/Window.java
@@ -9,6 +9,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import tech.jimothy.db.Database;
+import tech.jimothy.db.DatabaseConfig;
 import tech.jimothy.db.Table;
 import tech.jimothy.gui.nav.Nav;
 
@@ -17,7 +18,7 @@ public class Window extends Application{
     @Override
     public void start(Stage stage) throws IOException, SQLException{
         //DATABASE TEST -------------------------------------------
-        Database database = new Database("./sqlite/inibase");
+        Database database = new Database(DatabaseConfig.URL);
         Table table = database.query("SELECT name FROM sqlite_schema WHERE type = 'table' AND name NOT LIKE 'sqlite_%';");
         for(int i = 0; i < table.getSize(); i++){
             System.out.println(table.get(i, "name"));
