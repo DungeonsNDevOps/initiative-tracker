aa2ab60adab3fd35594f64455a2075ef63918f7a
jimothy <timothynewton1998@gmail.com>
2023-09-20 15:55:42 -0400

    Added input validation and autoroll feature

--- src/main/java/tech/jimothy/gui/PrelimCombatController.java
+++ src/main/java/tech/jimothy/gui/PrelimCombatController.java
@@ -12,10 +12,11 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import tech.jimothy.db.DataShare;
 import tech.jimothy.db.Database;
+import tech.jimothy.db.DatabaseConfig;
 import tech.jimothy.db.Table;
 import tech.jimothy.errors.StageNotSetForNav;
-import tech.jimothy.gui.custom.CharacterWidget;
-import tech.jimothy.gui.custom.OptionCharacterWidget;
+import tech.jimothy.gui.custom.EntityWidget;
+import tech.jimothy.gui.custom.OptionEntityWidget;
 import tech.jimothy.gui.custom.SpotlightPane;
 import tech.jimothy.gui.nav.Nav;
 
@@ -44,40 +45,41 @@ public class PrelimCombatController {
         spotlightPane.setMinHeight(root.getPrefHeight());
 
 
-        Database database = new Database("./sqlite/inibase");
+        Database database = new Database(DatabaseConfig.URL);
         DataShare dataShare = DataShare.getInstance();
         int campaignID = dataShare.getInt();
         Table campaignsTable = database.query("SELECT * FROM campaigns");
         String campaignName = campaignsTable.get(campaignID - 1, "name");
-        Table charactersTable = database.query("SELECT * FROM characters WHERE " +
+        Table entitiesTable = database.query("SELECT * FROM entities WHERE " +
                                                 campaignName + " = 1");
         
-        for (int i = 0; i < charactersTable.getSize(); i++){
-            CharacterWidget character = new CharacterWidget(   0,
-                                                                charactersTable.get(i, "name"),
-                                                                Integer.valueOf(charactersTable.get(i, "bonus")),
-                                                                Integer.valueOf(charactersTable.get(i, "id"))
+        for (int i = 0; i < entitiesTable.getSize(); i++){
+            EntityWidget entity = new EntityWidget(   0,
+                                                                entitiesTable.get(i, "name"),
+                                                                Integer.valueOf(entitiesTable.get(i, "bonus")),
+                                                                Integer.valueOf(entitiesTable.get(i, "id")),
+                                                                entitiesTable.get(i, "type")
             );
-            spotlightPane.getChildren().add(character);
+            spotlightPane.getChildren().add(entity);
 
-            TextField bonusTextField = new TextField();
-            Label bonusLabel = new Label('+' + charactersTable.get(i, "bonus"));
+            TextField initTextField = new TextField();
+            Label bonusLabel = new Label('+' + entitiesTable.get(i, "bonus"));
             
-            character.getChildren().add(bonusTextField);
-            character.getChildren().add(bonusLabel);
+            entity.getChildren().add(initTextField);
+            entity.getChildren().add(bonusLabel);
             
-            bonusTextField.setPrefWidth(35.0);
+            initTextField.setPrefWidth(35.0);
 
             //* Sets the onkeypressed event handle function to the handle function of the */
             //* onkeypressed event handle function of SpotlightPane */
             //* This allows for SpotlightPane to shift children even when the input focus is currently */
             //* set on the particular textfield */
             //TODO: Review lambda expressions and how they work in the context of javafx events
-            bonusTextField.setOnKeyPressed(event -> {
-                bonusTextField.getParent().getParent().getOnKeyPressed().handle(event);
-                ((CharacterWidget)spotlightPane.getChildren() //get the children in the spotlightpane
+            initTextField.setOnKeyPressed(event -> {
+                initTextField.getParent().getParent().getOnKeyPressed().handle(event);
+                ((EntityWidget)spotlightPane.getChildren() //get the children in the spotlightpane
                                                .get(spotlightPane.getSpotlightIndex())) //get the node at the spotlight index and cast it to CharacterWidget
-                                               .getChildren() //get the children from the children of the characterwidget
+                                               .getChildren() //get the children from the entitywidget
                                                .get(2) // get the child at the second index, which is the text field
                                                .requestFocus(); // request that the text field get the input focus
             });
@@ -90,35 +92,39 @@ public class PrelimCombatController {
     }
 
     public void finish(ActionEvent event) throws IOException{
-        //instantiate an arraylist to hold our CharacterWidgets
-        ArrayList<Object> characterArray = new ArrayList<>();
-        ObservableList<Node> spotlightChildren = this.spotlightPane.getChildren();
-        DataShare dataShare = DataShare.getInstance();
 
-        for(Node child : spotlightChildren){
-            if (child instanceof CharacterWidget){
-                CharacterWidget charWidget = (CharacterWidget)child;
-                String initiative = ((TextField)charWidget.getChildren()
-                                                          .get(2))
-                                                          .getText();
-                OptionCharacterWidget killableChar = new OptionCharacterWidget(
-                                                                                    0,
-                                                                                    charWidget.getName(),
-                                                                                    charWidget.getBonus(),
-                                                                                    charWidget.getID());
-                //set the initiative for our killableChar
-                killableChar.setInitiative(Integer.valueOf(initiative));
-                characterArray.add(killableChar);
+        if(inputValid()){
+            //instantiate an arraylist to hold our CharacterWidgets
+            ArrayList<Object> entityArray = new ArrayList<>();
+
+            ObservableList<Node> spotlightChildren = this.spotlightPane.getChildren();
+            DataShare dataShare = DataShare.getInstance();
+
+            for(Node child : spotlightChildren){
+                if (child instanceof EntityWidget){
+                    EntityWidget entityWidget = (EntityWidget)child;
+                    String initiative = ((TextField)entityWidget.getChildren()
+                                                            .get(2))
+                                                            .getText();
+                    OptionEntityWidget optionEntityWidget = new OptionEntityWidget(0,
+                                                            entityWidget.getName(),
+                                                            entityWidget.getBonus(),
+                                                            entityWidget.getID(),
+                                                            entityWidget.getType());
+                    //set the initiative for our killableChar
+                    optionEntityWidget.setInitiative(Integer.valueOf(initiative));
+                    entityArray.add(optionEntityWidget);
+                }
             }
-        }
-        //pass our newly made array to our datashare singleton class for use later
-        dataShare.setArray(characterArray);
+            //pass our newly made array to our datashare singleton class for use later
+            dataShare.setArray(entityArray);
 
-        Stage stage = (Stage)this.root.getScene().getWindow();
-        this.root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/combat-page.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+            Stage stage = (Stage)this.root.getScene().getWindow();
+            this.root = FXMLLoader.load(getClass().getResource("/tech/jimothy/fxml/combat-page.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     public void goBack(ActionEvent event){
@@ -127,5 +133,44 @@ public class PrelimCombatController {
         } catch (IOException | StageNotSetForNav e) {
             e.printStackTrace();
         }
+    }
+
+    private boolean inputValid(){
+        ObservableList<Node> spotlightChildren = this.spotlightPane.getChildren();
+        boolean inputValid = true;
+        
+        for(Node child : spotlightChildren){
+            TextField initTextField = ((TextField)((EntityWidget)child).getChildren().get(2));
+            if(initTextField.getText().equals(null) || initTextField.getText().equals("")){
+                inputValid = false;
+                //TODO: Highlight EntityWidgets that are invalid
+            }
+        }
+
+        return inputValid;
+    }
+
+    /**
+     * Autorolls initiatives for the entities of the specified types
+     */
+    public void autoRoll(){
+        ObservableList<Node> spotlightChildren = this.spotlightPane.getChildren();
+        String [] types = {"monster", "npc"};
+
+        for(Node child : spotlightChildren){
+            TextField initTextField = ((TextField)((EntityWidget)child).getChildren().get(2));
+
+            for (String type : types){
+                if (((EntityWidget)child).getType().equals(type)){
+                    initTextField.setText(String.valueOf(rollD20()));
+                }
+            }
+
+        }
+        ((EntityWidget)spotlightPane.getSpotlightChild()).getChildren().get(2).requestFocus();
+    }
+    
+    private int rollD20(){
+        return (int)(Math.random() * 20) + 1;
     }
 }
