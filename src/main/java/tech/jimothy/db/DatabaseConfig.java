0d549dc1cfaa00488b0ef245c1e5959534f6eb09
jimothy <timothynewton1998@gmail.com>
2023-09-19 20:12:09 -0400

    added java class forDatabase configuration

---
+++ src/main/java/tech/jimothy/db/DatabaseConfig.java
@@ -0,0 +1,8 @@
+package tech.jimothy.db;
+
+public class DatabaseConfig {
+    
+    /**The directory where the database file will be generated/accessed */
+    public static final String URL = "../sqlite/database";
+
+}
