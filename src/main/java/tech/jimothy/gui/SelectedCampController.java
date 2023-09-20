aef7f19e501f47d5817003ec4c8e4d49489d41eb
jimothy <timothynewton1998@gmail.com>
2023-09-20 15:56:35 -0400

    Added functionality for add monster button

--- src/main/java/tech/jimothy/gui/SelectedCampController.java
+++ src/main/java/tech/jimothy/gui/SelectedCampController.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -13,12 +12,14 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import tech.jimothy.db.DataShare;
 import tech.jimothy.db.Database;
+import tech.jimothy.db.DatabaseConfig;
 import tech.jimothy.db.Table;
-import tech.jimothy.design.CharacterItem;
+import tech.jimothy.design.CharacterItemType;
+import tech.jimothy.design.MonsterItemType;
 import tech.jimothy.errors.StageNotSetForNav;
 import tech.jimothy.gui.custom.SearchAndSelectWidget;
 import tech.jimothy.gui.nav.Nav;
-import tech.jimothy.gui.custom.OptionCharacterWidget;
+import tech.jimothy.gui.custom.OptionEntityWidget;
 
 public class SelectedCampController {
     
@@ -26,11 +27,15 @@ public class SelectedCampController {
     private Stage stage;
     private Scene scene;
     @FXML private Parent root;
-    @FXML VBox charactersVBox;
+    @FXML VBox entityVBox;
     @FXML AnchorPane anchorPane;
 
     private VBox addCharacterWidget;
+    private VBox addMonsterWidget;
     private boolean addCharacterWidgetExists = false;
+    private boolean addMonsterWidgetExists = false;
+
+//*----------------------------------------Navigation--------------------------------------------- */
     /**The navigation object used to naviagting around the app */
     Nav navigation = Nav.getInstance();
 
@@ -79,21 +84,34 @@ public class SelectedCampController {
         });
     }
 
+    public void goToCreateMonster(ActionEvent event) throws IOException, StageNotSetForNav{
+        navigation.goToCreateMonster();
+        navigation.setLastPage(() -> {
+            try {
+                navigation.goToSelectedCampaignPage(DataShare.getInstance().getInt());
+            } catch (IOException | StageNotSetForNav e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
     public void goBack(){
         try {
             navigation.goToCampaignPage();
         } catch (IOException | StageNotSetForNav e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
 
-    public void addCharacter(ActionEvent event){
+//*--------------------------------------------------------------------------------------------- */
+
+    //TODO: Figure out how to make positioning of widget dynamic instead of hard coded
+    public void manifestAddCharacterWidget(ActionEvent event){
         if(!addCharacterWidgetExists){
-            SearchAndSelectWidget addCharacterWidget = new SearchAndSelectWidget(this.charactersVBox, new CharacterItem());
+            SearchAndSelectWidget addCharacterWidget = new SearchAndSelectWidget(this.entityVBox, new CharacterItemType());
             this.addCharacterWidget = addCharacterWidget; 
 
-            AnchorPane.setLeftAnchor(addCharacterWidget, 20.0);
+            AnchorPane.setLeftAnchor(addCharacterWidget, 30.0);
             AnchorPane.setBottomAnchor(addCharacterWidget, 65.0);
 
             anchorPane.getChildren().add(addCharacterWidget);
@@ -105,31 +123,52 @@ public class SelectedCampController {
             addCharacterWidgetExists = false;
         }
     }
+    //TODO: Figure out how to make positioning of widget dynamic instead of hard coded
+    public void manifestAddMonsterWidget(ActionEvent event){
+        if(!addMonsterWidgetExists){
+            SearchAndSelectWidget addMonsterWidget = new SearchAndSelectWidget(this.entityVBox, new MonsterItemType());
+            this.addMonsterWidget = addMonsterWidget; 
+
+            AnchorPane.setLeftAnchor(addMonsterWidget, 245.0);
+            AnchorPane.setBottomAnchor(addMonsterWidget, 65.0);
+
+            anchorPane.getChildren().add(addMonsterWidget);
+
+            this.addMonsterWidgetExists = true;           
+        } else{
+            anchorPane.getChildren().remove(this.addMonsterWidget);
+            
+            this.addMonsterWidgetExists = false;
+        }
+    }
 
     @FXML
     /**
      * Method is invoked when the scene graph is loaded by FXMLloader
      */
     protected void initialize(){
-        Database database = new Database("./sqlite/inibase");
+        Database database = new Database(DatabaseConfig.URL);
         DataShare dataShare = DataShare.getInstance();
         int campaignID = dataShare.getInt();
         //System.out.println(campaignID);
         Table campaignsTable = database.query("SELECT * FROM campaigns");
         String campaignName = campaignsTable.get(campaignID - 1, "name");
-        Table charactersTable = database.query("SELECT * FROM characters WHERE " +
+        Table entitiesTable = database.query("SELECT * FROM entities WHERE " +
                                                 campaignName + " = 1");
         
-        for (int i = 0; i < charactersTable.getSize(); i++){
-            String characterName = charactersTable.get(i, "name");
-            int characterBonus = Integer.valueOf(charactersTable.get(i, "bonus"));
-            int characterID = Integer.valueOf(charactersTable.get(i, "id"));
-            OptionCharacterWidget character = new OptionCharacterWidget(20, 
-                                                            characterName, characterBonus,
-                                                            characterID
+        for (int i = 0; i < entitiesTable.getSize(); i++){
+            String entityName = entitiesTable.get(i, "name");
+            int entityBonus = Integer.valueOf(entitiesTable.get(i, "bonus"));
+            int entityID = Integer.valueOf(entitiesTable.get(i, "id"));
+            String entityType = entitiesTable.get(i, "type");
+            OptionEntityWidget character = new OptionEntityWidget(20, 
+                                                            entityName, 
+                                                            entityBonus,
+                                                            entityID,
+                                                            entityType
                                                             );
             character.setPrefWidth(100);
-            this.charactersVBox.getChildren().add(character);
+            this.entityVBox.getChildren().add(character);
         }
         database.close();
     }
