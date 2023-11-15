7929cffbcceb209b27d1edd08d4b0d5964cb2a52
jimothy <timothynewton1998@gmail.com>
2023-11-15 14:03:06 -0500

    Fixed error in logic

--- src/main/java/tech/jimothy/utils/Integers.java
+++ src/main/java/tech/jimothy/utils/Integers.java
@@ -3,15 +3,18 @@ package tech.jimothy.utils;
 public class Integers {
 
     public static boolean isInteger(String s){
+
         for (int i = 0; i < s.length(); i++){
             if (i == 0 && s.charAt(i) == '-'){
                 if (s.length() == 1){
+                    //String just has '-'
                     return false;
-                }            
-            } else if(i == 0) {
-                continue;
+                } else if(i == 0) {
+                    continue;           
+                } 
             }
             if (!Character.isDigit(s.charAt(i))){
+                //Char at index i does not represent a digit
                 return false;
             }
         }
